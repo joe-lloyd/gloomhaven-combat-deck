299fbbd546f0367c5eba5ec6b3570421e5fd30ad
Joseph Lloyd <joe.lloyd@polestar.com>
2023-09-02 20:51:46 +0200

    fix position of the border

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
-import { Deck } from "./Deck";
-import { TopBar } from "./TopBar";
-import { DeckProvider } from "./contexts/DeckContext";
-import { DiscardPile } from "./DiscardPile";
+import {Deck} from "./Deck";
+import {TopBar} from "./TopBar";
+import {DeckProvider} from "./contexts/DeckContext";
+import {DiscardPile} from "./DiscardPile";
 
 function App() {
   return (
     <main>
       <DeckProvider>
-        <TopBar />
-        <DiscardPile />
-        <Deck />
+        <TopBar/>
+        <div className="cards-wrapper-outer">
+          <div className="cards-wrapper-inner">
+            <DiscardPile/>
+            <Deck/>
+          </div>
+        </div>
       </DeckProvider>
     </main>
   );
